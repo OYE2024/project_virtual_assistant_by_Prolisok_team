8b6258fbf78bbe35d0ebd5cc038268c022b5c684
Oleksandr Yesypenko <alex.iesypenko@gmail.com>
2024-11-07 23:27:10 +0000

    SetUp complited

--- BOT_V2/__init__.py
+++
@@ -1,2 +0,0 @@
-from packages.module_clasess.address_book import *
-from packages.module_func.basic_functions import *
--- build/lib/BOT_V2/packages/module_clasess/__init__.py
+++
@@ -1,4 +0,0 @@
-from .address_book import AddressBook, Field, Name, Phone, Birthday, Email, Record
-
-__all__ = ["AddressBook", "Field", "Name",
-           "Phone", "Birthday", "Email", "Record"]
